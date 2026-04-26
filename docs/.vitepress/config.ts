220486b07c7db215e76d67aa336b565468f12d0b
Guilherme <guilherme.santanaa06@gmail.com>
2026-04-26 17:33:23 -0300

    Updates

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "Snapport",
+  description: "Automação de portfólio via GitHub",
+  base: '/snapport/',
+
+  themeConfig: {
+    nav: [
+      { text: 'Início', link: '/' },
+      { text: 'Documentação', link: '/guia/instalacao' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Começando',
+        items: [
+          { text: 'Instalação', link: '/guia/instalacao' },
+          { text: 'Configuração do GitHub', link: '/guia/github' },
+          { text: 'Componentes', link: '/guia/componentes' },
+          { text: 'Stacks Compatíveis', link: '/guia/stacks' }
+        ]
+      },
+      {
+        text: 'Referência',
+        items: [
+          { text: 'API (Parâmetros)', link: '/guia/api' },
+          { text: 'Customização CSS', link: '/guia/estilizacao' }
+        ],
+      },
+      {
+        text: 'Comunidade',
+        items: [
+          { text: 'Roadmap', link: '/projeto/roadmap' },
+          { text: 'Como Contribuir', link: '/projeto/contribuir' }
+        ],
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/guilhermegodoydev/snapport' }
+    ],
+
+    footer: {
+      message: 'Lançado sob a Licença MIT.',
+    }
+  }
+})
